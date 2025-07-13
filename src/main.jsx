bb5fda9cbd8a4d7b644a54794d461c381683cd5e
mandarin1977 <pkm30106@gmail.com>
2025-07-13 19:58:25 +0900

    dfs

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/mymypp/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
+  }
+});
