cef82313ebf0e8bd9922f4b053369fb4528c071d
mandarin1977 <pkm30106@gmail.com>
2025-07-13 20:00:01 +0900

    fdsfd

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}>
+    <HashRouter basename={`${import.meta.env.VITE_PUBLIC_URL}`}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
