73b3ef6dc233bc260264e926c163cbf8a3735e04
mandarin1977 <pkm30106@gmail.com>
2025-07-16 21:37:47 +0900

    tes

--- src/App_02.jsx
+++ src/App_02.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Counter from "./Counter";
 import Cart from "./Cart";
-import Header from "./Header";
 import './css/common.css'
 
 function App() {
  return(
   <>
-  <Header/>
   <Counter/>
   <Cart/>
   </>
